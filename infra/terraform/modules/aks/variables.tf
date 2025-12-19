a3a0f357c80ed06a8f2c282714ad60f896617cdc
Jacob Qi <qiyu0001@gmail.com>
2025-12-20 10:44:46 +1100

    add power yaml file for sotp and start AKS

--- infra/terraform/modules/aks/variables.tf
+++ infra/terraform/modules/aks/variables.tf
@@ -12,10 +12,18 @@ variable "user1_count" { type = number }
 variable "user2_vm_size" { type = string }
 variable "user2_count" { type = number }
 
-variable "enable_autoscaler_user1" { type = bool }
+variable "enable_autoscaler_user1" { 
+  type    = bool
+  default = true
+}
+
 variable "user1_min_count" { type = number }
 variable "user1_max_count" { type = number }
 
-variable "enable_autoscaler_user2" { type = bool }
+variable "enable_autoscaler_user2" { 
+  type    = bool
+  default = true
+}
+
 variable "user2_min_count" { type = number }
 variable "user2_max_count" { type = number }
